f6e13aee4fff1c4cf16a0d84a3d1532016916f41
xushankun <lovexsk@qq.com>
2020-01-16 16:27:52 +0800

    redux TodoList应用

--- src/pages/pageC.js
+++ src/pages/pageC.js
@@ -5,8 +5,8 @@ class pageC extends Component{
     render () {
         return (
             <div>
-                <h1>我是子组件pageC</h1>
-                <h1>我的ID是：{this.props.match.params.id}</h1>
+                <h1>我是子组件pageC【路由ID是：{this.props.match.params.id}】</h1>
+                <h1>下面是基于redux的TodoList：</h1>
                 <TodoList></TodoList>
             </div>
         );
